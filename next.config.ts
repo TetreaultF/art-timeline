473ac640e2c84edfc79a8eb811c79f80ab9dc118
Félix Tétreault <felix-2.tetreault@etud.polymtl.ca>
2026-09-16 22:42:12 -0400

    fixing paths

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     output: "export",
     basePath: "/art-timeline",
-    assetPrefix: "/art-timeline/",
     images: {
         unoptimized: true,
     },
