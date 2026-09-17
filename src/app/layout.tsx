1fe98fed6f586aa99bf5a7cb8b314295aa679fb1
Félix Tétreault <felix-2.tetreault@etud.polymtl.ca>
2026-09-16 22:56:09 -0400

    fixed relative paths of images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,7 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Histoire de l'art",
   description: "Exploration de l'art au fil du temps",
+  icons : { icon: "/art-timeline/favicon.png", },
 };
 
 export default function RootLayout({
